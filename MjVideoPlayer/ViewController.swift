0aad1b62e5630d1a8f84cd2f5e4fb9a62849001b
syokenz <syokenz@gmail.com>
2017-08-26 18:09:49 +0900

    動画と画像の再生速度を調整

--- MjVideoPlayer/ViewController.swift
+++ MjVideoPlayer/ViewController.swift
@@ -20,35 +20,40 @@ class ViewController: NSViewController {
     var imageTimer: Timer!
     var logTimer: Timer!
     var counter = 0
+    var timerOffset = 1.5
 
     override func viewDidLoad() {
         super.viewDidLoad()
         logView.backgroundColor = NSColor.black
         logView.textColor = NSColor.green
         
-        let videoPath = Bundle.main.path(forResource: "test", ofType: "mp4")
+        //let videoPath = Bundle.main.path(forResource: "test", ofType: "mp4")
+        let videoPath = "/Users/shoken/Downloads/mondo/mondo006.mp4"
         let graphPath = Bundle.main.path(forResource: "loading", ofType: "gif")
         let graphAsset = NSImage(contentsOfFile: graphPath!)
+        //let graphAsset = NSImage(contentsOfFile: "/Users/shoken/Downloads/loading003.gif")
+        
+        
         graphView.image = graphAsset
-        graphView.layer?.backgroundColor = NSColor.black.cgColor
         graphView.animates = true
 
         
-        let avAsset = AVURLAsset(url: URL(fileURLWithPath: videoPath!))
+        let avAsset = AVURLAsset(url: URL(fileURLWithPath: videoPath))
         let playerItem = AVPlayerItem(asset: avAsset)
         
         videoPlayer = AVPlayer(playerItem: playerItem)
         playerView.player = videoPlayer
         playerView.showsFullScreenToggleButton = true
 
-        imageTimer = Timer.scheduledTimer(timeInterval: 1.7, target: self, selector: #selector(self.updateImage), userInfo: nil, repeats: true)
+        imageTimer = Timer.scheduledTimer(timeInterval: timerOffset, target: self, selector: #selector(self.updateImage), userInfo: nil, repeats: true)
         imageTimer.fire()
-        logTimer = Timer.scheduledTimer(timeInterval: 0.3, target: self, selector: #selector(self.updateLog), userInfo: nil, repeats: true)
+        logTimer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.updateLog), userInfo: nil, repeats: true)
         logTimer.fire()
     }
     
     override func viewDidAppear() {
         super.viewDidAppear()
+        graphView.layer?.backgroundColor = NSColor.black.cgColor
         self.view.window?.title = "Mj Video Player"
     }
     
@@ -60,18 +65,30 @@ class ViewController: NSViewController {
     
     func updateImage(tm: Timer) {
         NSLog("update")
-        let prefix = "o_img_"
-        let imageNum = String(format: "%05d", counter)
-        let imagePath = Bundle.main.path(forResource: prefix + imageNum, ofType: "png")
+        logView.insertText("update\r", replacementRange: NSMakeRange(-1, 0))
+        let prefix = "o_r_img_"
+        let imageNum = String(format: "%04d", counter)
+        //let imagePath = Bundle.main.path(forResource: prefix + imageNum, ofType: "png")
+        let imagePath = "/Users/shoken/Downloads/mondo_cascade/" + prefix + imageNum + ".png"
         NSLog(prefix + imageNum)
-        if imagePath != nil {
-            let imgAsset = NSImage(contentsOfFile: imagePath!)
-            NSLog(imagePath!)
+        logView.insertText(prefix + imageNum + "\r", replacementRange: NSMakeRange(-1, 0))
+        if FileManager.default.fileExists(atPath: imagePath) {
+            let imgAsset = NSImage(contentsOfFile: imagePath)
+            NSLog(imagePath)
+            logView.insertText(imagePath + "\r", replacementRange: NSMakeRange(-1, 0))
             imageView.image = imgAsset
         } else {
             NSLog("Image missing.")
         }
         counter = counter + 1
+        
+        if counter == 5 {
+            timerOffset = 2.0
+            imageTimer.invalidate()
+            imageTimer = Timer.scheduledTimer(timeInterval: timerOffset, target: self, selector: #selector(self.updateImage), userInfo: nil, repeats: true)
+            imageTimer.fire()
+        }
+        logView.insertText("timeOffset = " + String(timerOffset) + "\r", replacementRange: NSMakeRange(-1, 0))
     }
     
     func updateLog(tm: Timer) {
